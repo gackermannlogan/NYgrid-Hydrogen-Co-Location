068ff7572e080274a95f6b994fbbf85bd8b4de9c
gackermannlogan <60230277+gackermannlogan@users.noreply.github.com>
2024-11-01 16:17:07 -0400

    added baseline function to open data

--- OpenDataHydrogen.jl
+++ OpenDataHydrogen.jl
@@ -1,45 +1,51 @@
 # Author: Gabriela Ackermann Logan, Cornell University
-# Last modified: October 2024
+# Last modified: November 2024
 
 # Import relevant packages
-using DataFrames, CSV, Glob, Dates, Plots, Statistics, StatsPlots
+using DataFrames, CSV, Glob, Dates, Plots, Statistics, StatsPlots, MAT
 
-# Define Functions
-function OpenData(resultspath)
-    all_result_files = glob("HydrogenResults_date*.csv", resultspath) # Use Glob to find only Hydrogen Results 
+function OpenBaseline(resultspath)
+    npcc_new = CSV.read("/Users/ga345/Desktop/NYgrid-main/Data/npcc_new.csv", DataFrame)
+    npcc = filter(row -> row.zone != "NA", npcc_new)
+    npcc = sort(npcc, :zone)
+    bus_zonal = DataFrame(bus_id = npcc.idx, zone =npcc.zone)
+    bus_to_zone = Dict(row.bus_id =>row.zone for row in eachrow(bus_zonal))
 
-    # all_data will store all the results (combine all CSV files)
-    all_data = DataFrame()
-    # Iterate through all files and ensure types are consistent
-    for file in all_result_files
-        data = CSV.read(file, DataFrame)
-        
-        # Ensure that 'MWFromGrid' column is Float64 in each file
-        if "MWFromGrid" in names(data)
-            data[!, :MWFromGrid] = convert(Vector{Float64}, data[!, :MWFromGrid])
+    mat_files = readdir(results_path; join=true)
+
+    # Initialize an empty DataFrame to store all data with timestamps
+    Fuel_data = DataFrame(timestamp = DateTime[], zone = String[], fuel = String[], Total_Power = Float64[])
+
+    for file_path in mat_files
+        if occursin(r"resultOPF.*\.mat", file_path)  # Only process .mat files that match the pattern
+            # Extract the timestamp from the filename 
+            filename = basename(file_path)
+            timestamp_str = match(r"\d{8}_\d{2}", filename).match  # Extract 'yyyymmdd_hh'
+            timestamp = DateTime(timestamp_str, "yyyymmdd_HH")
+
+            # Open the .mat file and read data
+            file = matopen(file_path)
+            data = read(file, "resultOPF")
+            close(file)
+            
+            gen_data = data["gen"]
+            fueltype = data["genfuel"]
+            df = DataFrame(bus_id = gen_data[:,1], power = gen_data[:,2], fuel = fueltype[:])
+            df = sort(df, :bus_id)
+            matching = filter(row -> row.bus_id in bus_zonal.bus_id, df)
+            matching.zone = [bus_to_zone[row.bus_id] for row in eachrow(matching)]
+            grouped_matching = combine(groupby(matching, [:zone, :fuel]),
+                                :power => sum => :Total_Power)
+            grouped_matching.timestamp .=timestamp
+            append!(Fuel_data, grouped_matching)
         end
-        
-        # Append the data
-        append!(all_data, data, promote=true)
     end
-
-    all_data[!, :Timestamp] = fix_timestamp.(String.(all_data.Timestamp))
-    all_data[!, :Timestamp] = replace_month.(all_data.Timestamp)  # Replace month abbreviations
-
-    # Convert the Timestamp column to datetime for easier grouping by months
-    try
-        all_data[!, :Timestamp] = Dates.DateTime.(all_data.Timestamp, "dd-mm-yyyy HH:MM:SS")
-        println("Successfully converted all timestamps to DateTime.")
-    catch e
-        println("Error converting timestamps: ", e)
-    end
-
-    return all_data
+    return Fuel_data 
 end
 
 # Define Functions
-function OpenData_Scenario2(resultspath)
-    all_result_files = glob("HydrogenResults_Scenario2_date*.csv", resultspath) # Use Glob to find only Hydrogen Results 
+function OpenDataScenario(resultspath, scenario)
+    all_result_files = glob("HydrogenResults_Scenario$(scenario)_date*.csv", resultspath) # Use Glob to find only Hydrogen Results 
 
     # all_data will store all the results (combine all CSV files)
     all_data = DataFrame()
@@ -66,12 +72,34 @@ function OpenData_Scenario2(resultspath)
     catch e
         println("Error converting timestamps: ", e)
     end
-    
+
     return all_data
 end
 
 function OpenFuelData(resultspath)
     Fuel_mix_files = glob("BaslineFuelMix_*.csv", resultspath)  # Use Glob to find only Hydrogen Results
+    # Create one DataFrame to store all the results for Fuel
+    all_Fuel_data = DataFrame()
+    for file in Fuel_mix_files
+        data = CSV.read(file, DataFrame)
+        append!(all_Fuel_data, data)
+    end
+
+    # Convert 'TimeStamp' in all_Fuel_data from string to DateTime
+    all_Fuel_data.TimeStamp = String.(all_Fuel_data.TimeStamp)
+    all_Fuel_data[!, :TimeStamp] = Dates.DateTime.(all_Fuel_data.TimeStamp, "mm/dd/yyyy HH:MM:SS")
+
+    all_Fuel_data1 = sort(all_Fuel_data, :TimeStamp)
+    all_Fuel_data= filter(row ->row.TimeStamp !="2020-01",all_Fuel_data1)
+
+    # Remove duplicates from the final FuelMix
+    unique!(all_Fuel_data)
+    return all_Fuel_data
+end
+
+function OpenFuelDataScenario(resultspath, scenario, all_data)
+   # Fuel_mix_files = glob("HydrogenFuelMix_Scenario$(scenario)*.csv", resultspath)  # Use Glob to find only Hydrogen Results
+   Fuel_mix_files = glob("HydrogenFuelMix*.csv", resultspath)
     # Create one DataFrame to store all the results for Fuel
     all_Fuel_data = DataFrame()
     for file in Fuel_mix_files
@@ -134,8 +162,4 @@ function replace_month(ts::String)
     end
     return ts  # Return the timestamp if no replacement is needed
 end
-
-
-
-
 
